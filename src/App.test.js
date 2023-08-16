96f9ed56ebd8d52c9d87c2aa1b49cf3307883dc8
numbafye <saadiqgp@gmail.com>
2023-08-16 18:37:49 -0400

    snapshot test works

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import renderer from 'react-test-renderer'
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import App from "./App";
 
-test('renders a snapshot', () => {
-    const tree = renderer.create(<App/>).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+test("renders a snapshot", () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
